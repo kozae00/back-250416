2ca4b0c5d25e09790613ff536908f8ed3177f1fd
kozae00 <rhwogus777@gmail.com>
2025-04-16 14:10:24 +0900

    work2

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    // https://mvnrepository.com/artifact/io.awspring.cloud/spring-cloud-aws-starter-s3
     implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:3.3.0")
 }
 
