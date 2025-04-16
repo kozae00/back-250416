08a0ff5dadc3f7613c1d98e9b62b119e4d98f4d2
chacha86 <taejincha0619@gmail.com>
2025-04-16 10:11:34 +0900

    init

---
+++ src/test/kotlin/com/think/back2/Back2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.think.back2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Back2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
